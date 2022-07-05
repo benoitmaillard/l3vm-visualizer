bc7a40f397f06d26e09bac5665f109be6916f23e
Benoît <benoit.maillard@epfl.ch>
2022-07-05 17:53:31 +0200

    Add GridManager class to handle everything related to drawing

---
+++ src/main/scala/tutorial/webapp/GridManager.scala
@@ -0,0 +1,47 @@
+package tutorial.webapp
+
+import org.scalajs.dom
+
+class GridManager(
+    canvas: dom.html.Canvas,
+    memRep: SpatialMemoryRepresentation,
+    squareWidth: Int
+) {
+  val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+
+  def setup() = {
+    // TODO include space for margins (for drawing region boundaries)
+    canvas.width = memRep.width * squareWidth
+    canvas.height = memRep.height * squareWidth
+  }
+
+  def processEventSeq(s: Seq[TraceEvent]): Unit = {
+    clear()
+    s.zipWithIndex.foreach(processEvent)
+  }
+
+  private def processEvent(e: TraceEvent, pos: Int): Unit = e match {
+    case MemoryRead(address) =>
+      drawWord(address, GridManager.ReadColor.copy(a = 1.0f / pos))
+    case MemoryWrite(address) =>
+      drawWord(address, GridManager.WriteColor.copy(a = 1.0f / pos))
+  }
+
+  private def clear(): Unit = ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  private def drawWord(address: Int, color: GridManager.Color): Unit = {
+    val (row, col) = memRep.addressToSquare(address)
+    drawSquare(row, col, color)
+  }
+
+  private def drawSquare(row: Int, col: Int, color: GridManager.Color): Unit = {
+    ctx.fillStyle = f"rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})"
+    ctx.fillRect(squareWidth * col, squareWidth * row, squareWidth, squareWidth)
+  }
+}
+
+object GridManager {
+  case class Color(r: Int, g: Int, b: Int, a: Float)
+  val ReadColor = Color(0, 0, 255, 1.0)
+  val WriteColor = Color(0, 255, 0, 1.0)
+}
