043fb6d760b2ec8893ccda1c99852102d687e2d7
Benoît <benoit.maillard@epfl.ch>
2022-07-05 17:46:43 +0200

    Fix addressToSquare in scan representation

--- src/main/scala/tutorial/webapp/SpatialMemoryRepresentation.scala
+++ src/main/scala/tutorial/webapp/SpatialMemoryRepresentation.scala
@@ -1,6 +1,6 @@
 package tutorial.webapp
 
-abstract class SpatialMemoryRepresentation(width: Int, height: Int) {
+abstract class SpatialMemoryRepresentation(val width: Int, val height: Int) {
   def addressToSquare(address: Int): (Int, Int)
   def squareToAddress(row: Int, col: Int): Int
 }
@@ -8,10 +8,10 @@ abstract class SpatialMemoryRepresentation(width: Int, height: Int) {
 class ScanMemoryRepresentation(width: Int, height: Int)
     extends SpatialMemoryRepresentation(width, height) {
   override def addressToSquare(address: Int): (Int, Int) = {
-    val row = address / height
+    val row = address / width
     val posInRow = address % width
     val col = if row % 2 == 0 then posInRow else width - posInRow
-    (col, row)
+    (row, col)
   }
   override def squareToAddress(row: Int, col: Int): Int = {
     val posInRow = if row % 2 == 0 then col else width - col
