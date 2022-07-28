52fe2810dc9e3f976e0d449fc060ef26354e87db
Benoît <benoit.maillard@epfl.ch>
2022-07-28 18:03:00 +0200

    Fix bugs related to the animation and improve code

--- src/main/scala/visualizer/webapp/BinaryTrace.scala
+++ src/main/scala/visualizer/webapp/BinaryTrace.scala
@@ -13,28 +13,47 @@ import scala.scalajs.js.typedarray._
 
 import js.JSConverters._
 
+class TraceIndex(clockUrl: String) {
+  private val file = new ChunkedFile(clockUrl)
+
+  def read(from: Long, length: Int): Future[Iterator[(Int, Int)]] = {
+    val rangeStart = if from > 0 then from - 1 else 0
+    val rangeLength = if from > 0 then length + 1 else length
+    file.read(rangeStart * TraceIndex.EntryBytes, rangeLength * TraceIndex.EntryBytes) map { bytes =>
+      val extracted = bytes.grouped(TraceIndex.EntryBytes).map(extractIndex)
+      val complete = if from > 0 then extracted else Iterator(0) ++ extracted
+      complete.sliding(2).withPartial(false).map(a => (a(0), a(1)))
+    }
+  }
+
+  def length(): Future[Long] = file.length().map(_ / TraceIndex.EntryBytes)
+
+  private def extractIndex(s: Seq[Short]) =
+    (s(0) << 24) | (s(1) << 16) | (s(2) << 8) | s(3)
+}
+
+object TraceIndex {
+  val EntryBytes = 4
+}
+
 case class BinaryTrace(url: String, clockUrl: String) extends ProgramTrace {
   private val file = new ChunkedFile(url)
-  private val clockFile = new ChunkedFile(clockUrl)
+  private val clockFile = new TraceIndex(clockUrl)
 
   def read(
       from: Long,
       length: Int,
       forward: Boolean = true
   ): Future[Seq[Seq[TraceEvent]]] = {
-    val t = System.currentTimeMillis()
-    val clockFirst = math.max(0, (from - length) * BinaryTrace.IndexEntryBytes)
-    val clockLength = (length+1) * BinaryTrace.IndexEntryBytes
-    clockFile.read(clockFirst, clockLength).flatMap { bytes =>
-      val indexes = bytes.grouped(BinaryTrace.IndexEntryBytes).map(extractIndex).toSeq
-      val indexesNew = if indexes.size < 2 then 0 +: indexes else indexes
-      val ranges = indexesNew.init zip indexesNew.tail
+    val tMin = math.max(0, from - length + 1)
+    val tLength = math.min(from + 1, length).toInt
+    clockFile.read(tMin, tLength).flatMap { ranges =>
       val futures = ranges.map((from, to) => file.read(from * BinaryTrace.EventBytes, (to - from) * BinaryTrace.EventBytes).map(s => s.grouped(BinaryTrace.EventBytes).map(extractEvent).toSeq))
-      Future.sequence(futures.reverse)
+      Future.sequence(futures.toSeq.reverse)
     }
   }
 
-  def length(): Future[Long] = clockFile.length().map(_ / BinaryTrace.IndexEntryBytes)
+  def length() = clockFile.length()
 
   private def extractEvent(s: Seq[Short]) =
     val clockBit = s(0) & (1 << 7)
@@ -44,12 +63,8 @@ case class BinaryTrace(url: String, clockUrl: String) extends ProgramTrace {
       case 0 => MemoryRead(address)
       case 1 => MemoryWrite(address)
     }
-
-  private def extractIndex(s: Seq[Short]) =
-    (s(0) << 24) | (s(1) << 16) | (s(2) << 8) | s(3)
 }
 
 object BinaryTrace {
   val EventBytes = 3
-  val IndexEntryBytes = 4
 }
